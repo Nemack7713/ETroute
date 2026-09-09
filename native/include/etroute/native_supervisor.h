63d6c8154157980a86e6f1623c033f13002e6583
Nemack7713 <esgmacc@gmail.com>
2026-09-08 21:57:01 -0400

    Harden native supervisor result contract

--- native/include/etroute/native_supervisor.h
+++ native/include/etroute/native_supervisor.h
@@ -6,6 +6,23 @@
 
 namespace etroute {
 
+enum class SpawnStage : int {
+    Ok = 0,
+    InvalidArgument = 1,
+    OpenStdout = 2,
+    OpenStderr = 3,
+    ErrorPipe = 4,
+    Fork = 5,
+    Setpgid = 6,
+    ResourceLimit = 7,
+    Chdir = 8,
+    Dup2Stdout = 9,
+    Dup2Stderr = 10,
+    Execve = 11,
+    Waitpid = 12,
+    TimeoutKill = 13,
+};
+
 struct NativeResourceLimits {
     std::uint64_t cpu_seconds = 0;
     std::uint64_t max_open_files = 0;
@@ -29,10 +46,15 @@ struct NativeRunResult {
     int signal = 0;
     bool timed_out = false;
     int spawn_errno = 0;
+    SpawnStage stage = SpawnStage::Ok;
     std::int64_t duration_ms = 0;
 
     [[nodiscard]] bool succeeded() const noexcept {
-        return !timed_out && spawn_errno == 0 && signal == 0 && exit_code == 0;
+        return stage == SpawnStage::Ok &&
+               !timed_out &&
+               spawn_errno == 0 &&
+               signal == 0 &&
+               exit_code == 0;
     }
 };
 
